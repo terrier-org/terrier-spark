5c89faf886263c05f6f5a2a7778cf8ed3091ed48
Craig Macdonald <craig.macdonald_AT_glasgow.ac.uk>
2018-05-03 15:53:55 +0100

    this is used

---
+++ src/main/scala/org/terrier/spark/ml/LabelStageTrait.scala
@@ -0,0 +1,15 @@
+package org.terrier.spark.ml
+
+import org.apache.spark.ml.PipelineStage
+import org.apache.spark.ml.param.Param
+import org.apache.spark.ml.param.Params
+
+trait LabelStageTrait extends Params {
+  val uid: String = "LabelStageTrait"
+  
+  val outputLabelCol= new Param[String](this, "outputLabelCol", "The output column containing the (relevance) label")
+  def setOutputLabelCol(value: String): this.type = set(outputLabelCol, value)
+  
+  setDefault(outputLabelCol, "label")
+  
+}
